74899ecdd19ff8684101d2a3c3077d1d28a4f77e
leo <leonardo.sibani@gmail.com>
2019-10-13 23:32:12 +0200

    file inside src-snakegame-files wasnt gonna update itself when zipped in jar. set the file to be created in user home dir

--- src/snake/game/Scores.java
+++ src/snake/game/Scores.java
@@ -4,20 +4,22 @@ import java.io.File;
 import java.io.FileWriter;
 
 public class Scores {
+
     private File file;
     private FileWriter writer;
-    private static String path = "src/snake/game/files/scores.txt";
-    
-    public Scores(){
-        this.file = new File(path);
+    private static final String FILENAME = "SnakeGameScores.txt";
+    private static final String PATH = System.getProperty("user.home");
+
+    public Scores() {
+        this.file = new File(PATH + File.separator + FILENAME);
     }
-    
-    public void addLastScore(int score){
+
+    public void addLastScore(int score) {
         try {
-        this.writer = new FileWriter(path, true);
-        this.writer.append("\n" + score);
-        this.writer.close();
-        } catch (Exception e){
+            this.writer = new FileWriter(this.file, true);
+            this.writer.append("\n" + score);
+            this.writer.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
